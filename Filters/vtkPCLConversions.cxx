e0dbc7423651018648d3bbe02d4da9e67174dd40
Michal Staniaszek <m.staniaszek@gmail.com>
2021-02-12 10:49:25 +0000

    use pcl library to do conversion to vtk, avoiding stack smash issue in noetic

--- Filters/vtkPCLConversions.cxx
+++ Filters/vtkPCLConversions.cxx
@@ -30,6 +30,7 @@
 
 #include <pcl/io/pcd_io.h>
 #include <pcl/common/transforms.h>
+#include <pcl/io/vtk_lib_io.h>
 
 #include <cassert>
 
@@ -48,46 +49,6 @@ vtkPCLConversions::~vtkPCLConversions()
 
 
 //----------------------------------------------------------------------------
-
-/**
- * @brief The PointCloudFieldIterator class is used to iterate through a PCLPointCloud2
- */
-class PointCloudFieldIterator
-{
-public:
-  PointCloudFieldIterator(pcl::PCLPointCloud2& cloud, const std::string& field_name)
-  {
-    for (size_t i = 0; i < cloud.fields.size(); ++i)
-    {
-      if(cloud.fields[i].name == field_name)
-      {
-        int offset = cloud.fields[i].offset;
-        data_char_ = &(cloud.data.front()) + offset;
-        data_ = reinterpret_cast<float*>(data_char_);
-        point_step_ = cloud.point_step;
-        return;
-      }
-    }
-    throw std::runtime_error("Field " + field_name + " does not exist");
-
-  }
-
-  PointCloudFieldIterator& operator ++()
-  {
-    data_char_ += point_step_;
-    data_ = reinterpret_cast<float*>(data_char_);
-  }
-
-  float& operator*()
-  {
-    return *data_;
-  }
-
-private:
-  unsigned char* data_char_;
-  float* data_;
-  int point_step_;
-};
 
 vtkSmartPointer<vtkPolyData> vtkPCLConversions::PolyDataFromPCDFile(const std::string& filename)
 {
@@ -126,44 +87,9 @@ vtkSmartPointer<vtkPolyData> vtkPCLConversions::PolyDataFromPCDFile(const std::s
 
 vtkSmartPointer<vtkPolyData> vtkPCLConversions::ConvertPointCloud2ToVtk(pcl::PCLPointCloud2& cloud)
 {
-  const size_t numberOfPoints = cloud.height*cloud.width;
-
   vtkSmartPointer<vtkPolyData> polyData = vtkSmartPointer<vtkPolyData>::New();
-  vtkSmartPointer<vtkPoints> points = vtkSmartPointer<vtkPoints>::New();
-  points->SetDataTypeToFloat();
-  points->Allocate(numberOfPoints);
-  polyData->SetPoints(points);
-  polyData->SetVerts(NewVertexCells(numberOfPoints));
-
-  for (size_t i = 0; i < cloud.fields.size(); ++i)
-  {
-    std::string field_name = cloud.fields[i].name;
-    //points (x,y,z) are read from the x field only so y and z can be ignored
-    if (field_name == "y" || field_name == "z")
-      continue;
-
-    if (field_name == "x")
-    {
-      PointCloudFieldIterator iter(cloud, field_name);
-      for (size_t j = 0; j < numberOfPoints; ++j, ++iter)
-      {
-        points->InsertNextPoint(&(*iter));
-      }
-    } else
-    {
-      vtkSmartPointer<vtkFloatArray> field = vtkSmartPointer<vtkFloatArray>::New();
-      field->SetName(field_name.c_str());
-      field->SetNumberOfValues(numberOfPoints);
-      polyData->GetPointData()->AddArray(field);
-      PointCloudFieldIterator iter(cloud, field_name);
-      for (size_t j = 0; j < numberOfPoints; ++j, ++iter)
-      {
-        field->SetValue(j, *iter);
-      }
-    }
-
-  }
-
+  pcl::PCLPointCloud2Ptr cloudPtr = boost::make_shared<pcl::PCLPointCloud2>(cloud);
+  pcl::io::pointCloudTovtkPolyData(cloudPtr, polyData);
   return polyData;
 }
 
